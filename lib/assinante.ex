23509e6a55b9855b21f3fd7ae9e032a1eac24610
Marcos <marcosvto1@gmail.com>
2022-03-02 20:12:08 -0300

    clean functions

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -9,22 +9,32 @@ defmodule Assinante do
         |> :erlang.term_to_binary()
         |> write(plano) 
         {:ok, "Assiante #{nome} cadastrado com sucesso"}
+
       _assinante -> {:error, "Assinante com este número cadastro"}
     end  
   end
 
-  def buscar_assinante(numero) do
-    read(:prepago) ++ read(:pospago)
-    |> Enum.find(fn assinante -> assinante.numero == numero end)
-  end
+  def buscar_assinante(numero, key \\ :all), do: buscar(numero, key)
 
-  defp write(lista_assinantes, plano) do
-    File.write!(@assinantes[plano], lista_assinantes)
-  end
+  defp buscar(numero, :prepago), do: filtro(assinante_prepago(), numero)
+  defp buscar(numero, :pospago), do: filtro(assinante_pospago(), numero)
+  defp buscar(numero, :all), do: filtro(assinantes(), numero)
+  defp filtro(lista, numero), do: Enum.find(lista, &(&1.numero == numero))
+
+  def assinante_prepago, do: read(:prepago)
+
+  def assinante_pospago, do: read(:pospago)
+
+  def assinantes, do: read(:prepago) ++ read(:pospago)
+
+  defp write(lista_assinantes, plano), do: File.write!(@assinantes[plano], lista_assinantes)
 
   def read(plano) do
-    {:ok, assinantes} = File.read(@assinantes[plano])
-    assinantes
-    |> :erlang.binary_to_term()
+    case File.read(@assinantes[plano]) do
+      {:ok, assinantes} -> 
+        assinantes
+        |> :erlang.binary_to_term()
+      {:error, :ennoent} -> {:error, "Arquivo inválido"}
+    end
   end
 end
